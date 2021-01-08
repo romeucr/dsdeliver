0791f4e2c45a998ddaa26b2eb1045bf483cce62b
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2021-01-08 16:30:16 +0100

    Search order location

---
+++ front-web/src/api.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_URL = 'http://192.168.0.18:8080'
+
+//utilizando o token do mapbox que está armazenado na var de ambiente em .env
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX
+
+export function fetchProducts() {
+   return axios(`${API_URL}/products`)
+}
+
+export function fetchLocalMapBox(local: string) {
+   return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}
+
+   `)
+}
