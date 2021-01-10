faf5250566d8f09de3dba740c0f8bcfa6ff167d5
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2021-01-10 19:35:28 +0100

    Add Orders page

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,31 +1,41 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 function Header() {
+
+  const navigation = useNavigation()
+  const handleOnPress = () => {
+     navigation.navigate('Home')
+  }
+  //TouchableWithoutFeedback: por padrão as views nao sao clicaveis. Esse componente permite capturar o evento onPress 
   return (
-    <View style={styles.container}>
-       <Image source={require('../assets/logo.png')} />
-      <Text style={styles.text}>DS Delivery</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={handleOnPress}> 
+      <View style={styles.container}>
+        <Image source={require('../assets/logo.png')} />
+        <Text style={styles.text}>DS Delivery</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-     backgroundColor: '#DA5C5C',
-     height: 90,
-     paddingTop: 50,
-     flexDirection: 'row',
-     justifyContent: 'center'
+    backgroundColor: '#DA5C5C',
+    height: 90,
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'center'
   },
   text: {
-   fontWeight: 'bold',
-   fontSize: 18,
-   lineHeight: 25,
-   letterSpacing: -0.024,
-   color: '#FFF',
-   marginLeft: 15,
-   fontFamily: 'OpenSans_700Bold'
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 25,
+    letterSpacing: -0.024,
+    color: '#FFF',
+    marginLeft: 15,
+    fontFamily: 'OpenSans_700Bold'
   }
 });
 
